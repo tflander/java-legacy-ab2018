35eb9b27677938da0ffcebce40c09078ba2215b7
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-05-18 22:53:01 -0400

    extract application

--- src/main/java/com/neopragma/legacy/screen/JobApplicant.java
+++ src/main/java/com/neopragma/legacy/screen/JobApplicant.java
@@ -112,44 +112,12 @@ public class JobApplicant {
 		save();
 	}
 	
-	private void save() {
+	public void save() {
 		//TODO save information to a database
 		System.out.println("Saving to database: " + formatLastNameFirst());
 	}
-	
-	public static void main(String[] args) throws URISyntaxException, IOException {
-		JobApplicant jobApplicant = new JobApplicant();
-		Scanner scanner = new Scanner(System.in);
-		String firstName;
-		String middleName;
-		String lastName;
-		String ssn;
-		String zipCode;
-		while (true) {
-			System.out.println("Please enter info about a job candidate or 'quit' to quit");
-			System.out.println("First name?");
-            firstName = scanner.nextLine();		
-            if (firstName.equals("quit")) {
-            	scanner.close();
-            	System.out.println("Bye-bye!");
-            	break;
-            }
-			System.out.println("Middle name?");
-            middleName = scanner.nextLine();
-			System.out.println("Last name?");
-            lastName = scanner.nextLine();			
-			System.out.println("SSN?");
-            ssn = scanner.nextLine();			
-			System.out.println("Zip Code?");
-            zipCode = scanner.nextLine();			
-            jobApplicant.setName(firstName, middleName, lastName);          
-            jobApplicant.setSsn(ssn);
-            jobApplicant.setAddress(jobApplicant.populateUsingZipCode(zipCode));
-            jobApplicant.save();
-		}
-	}
 
-    private Address populateUsingZipCode(String zipCode) throws IOException, URISyntaxException {
+    public Address populateAddressUsingZipCode(String zipCode) throws IOException, URISyntaxException {
         return addressProvider.populateUsingZipCode(zipCode);
     }
 
---
+++ src/main/java/com/neopragma/legacy/screen/JobApplicantApp.java
@@ -0,0 +1,41 @@
+package com.neopragma.legacy.screen;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
+public class JobApplicantApp {
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        JobApplicant jobApplicant = new JobApplicant();
+        Scanner scanner = new Scanner(System.in);
+        String firstName;
+        String middleName;
+        String lastName;
+        String ssn;
+        String zipCode;
+        while (true) {
+            System.out.println("Please enter info about a job candidate or 'quit' to quit");
+            System.out.println("First name?");
+            firstName = scanner.nextLine();
+            if (firstName.equals("quit")) {
+                scanner.close();
+                System.out.println("Bye-bye!");
+                break;
+            }
+            System.out.println("Middle name?");
+            middleName = scanner.nextLine();
+            System.out.println("Last name?");
+            lastName = scanner.nextLine();
+            System.out.println("SSN?");
+            ssn = scanner.nextLine();
+            System.out.println("Zip Code?");
+            zipCode = scanner.nextLine();
+            jobApplicant.setName(firstName, middleName, lastName);
+            jobApplicant.setSsn(ssn);
+            jobApplicant.setAddress(jobApplicant.populateAddressUsingZipCode(zipCode));
+            jobApplicant.save();
+        }
+    }
+
+}
